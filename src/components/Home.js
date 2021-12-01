3a7aac71fa46fcf3097c34505caca4d2a952202c
Jonathan <jonathantoledo11@gmail.com>
2021-11-30 17:37:04 -0800

    commit tree fa79ccc645bcf949f60bce17de7e7db0ca601793

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import name from "../src/data";
+import city from "city";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
-}
+  return (
+    <div id="home">
+      <h1 style={{color: "firebrick"}}> Name is a Web Developer from City</h1>
+    </div>
+    
+  );
+};
 
 export default Home;
